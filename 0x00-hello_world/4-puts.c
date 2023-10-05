1e7cb35aeb652446fb57e0407a3ed5f3047dd8b5
mo-abdel-samie <mohamed.abdelsamie3009@gmail.com>
2023-10-05 03:46:48 -0700

    m

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function for puts a string
+ * Description: print string with new line
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
