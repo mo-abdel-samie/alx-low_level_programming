11ee580f320e040bb2f5ebc05642bfcc928256e5
mo-abdel-samie <mohamed.abdelsamie3009@gmail.com>
2023-10-05 04:07:38 -0700

    m

---
+++ 0x00-hello_world/01-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function that make proces
+ * Description: function for make proces
+ * Return: 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
