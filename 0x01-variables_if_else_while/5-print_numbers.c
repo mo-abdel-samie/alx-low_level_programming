df4d6355a407a3c0eacc86d084b117ea13702b78
mo-abdel-samie <mohamed.abdelsamie3009@gmail.com>
2023-10-07 08:51:53 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function that make proces
+ * Description: function for make proces
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
