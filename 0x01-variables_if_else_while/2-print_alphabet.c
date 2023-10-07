e1f60f229684c60022c1c60a88094790edba7bdb
mo-abdel-samie <mohamed.abdelsamie3009@gmail.com>
2023-10-07 06:59:15 -0700

    m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function that make proces
+ * Description: function for make proces
+ * Return: 0
+ */
+int main(void)
+{
+	char sch;
+
+	for (sch = 'a'; sch <= 'z'; sch++)
+	{
+		putchar(sch);
+	}
+
+	return (0);
+}
