580547754a4e1f065a254ef97b447ca6ff5dd5ed
mo-abdel-samie <mohamed.abdelsamie3009@gmail.com>
2023-10-07 08:59:10 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function that make proces
+ * Description: function for make proces
+ * Return: 0
+ */
+int main(void)
+{
+	int sch;
+
+	for (sch = '0'; sch <= '9'; sch++)
+	{
+		putchar(sch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
