e207a04e97a5d0a8964d594124d90a5e35f42f97
mo-abdel-samie <mohamed.abdelsamie3009@gmail.com>
2023-10-13 06:54:11 -0700

    m

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,18 +14,20 @@ void print_square(int n)
 	if (n > 0)
 	{
 		int replay;
+		
 		for (replay = 1; replay <= n; replay++)
 		{
 			int count;
+			
 			for (count = 1; count <= n; count++)
 			{
 				_putchar(35);
 			}
-		_putchar('\n');
+			_putchar('\n');
 		}
 	}
 	else
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
