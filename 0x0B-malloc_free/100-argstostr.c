99b06b966d342205f3bd18d9915a2db8d2920af5
mo-abdel-samie <mohamed.abdelsamie3009@gmail.com>
2023-10-30 14:34:46 -0700

    m

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,33 +9,36 @@
 
 char *argstostr(int ac, char **av)
 {
-		char *new_string = NULL;
-		int k = 0, i = ac, j, sum = 0, temp = 0;
+	char *new_string = NULL;
+	int k = 0, i = ac, j, sum = 0, temp = 0;
 
-		if (ac == 0 || av == NULL)
-			return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-		while (ac--)
-			sum += (len(av[ac]) + 1);
-		new_string = (char *) malloc(sum + 1);
+	while (ac--)
+		sum += (len(av[ac]) + 1);
 
-		if (new_string != NULL)
+	new_string = (char *) malloc(sum + 1);
+
+	if (new_string != NULL)
+	{
+		while (k < i)
 		{
-			while (k < i)
-			{
-				for (j = 0; av[k][j] != '\0'; j++)
-					new_string[j + temp] = av[k][j];
-				new_string[temp + j] = '\n';
-				temp += (j + 1);
-				k++;
-			}
-			new_string[temp] = '\0';
+			for (j = 0; av[k][j] != '\0'; j++)
+				new_string[j + temp] = av[k][j];
+
+			new_string[temp + j] = '\n';
+			temp += (j + 1);
+			k++;
 		}
-		else
-		{
-			return (NULL);
-		}
-		return (new_string);
+		new_string[temp] = '\0';
+	}
+	else
+	{
+		return (NULL);
+	}
+
+	return (new_string);
 }
 
 /**
@@ -45,12 +48,12 @@ char *argstostr(int ac, char **av)
 */
 int len(char *str)
 {
-		int len = 0;
+	int len = 0;
 
-		if (str != NULL)
-		{
-			while (str[len])
-				len++;
-		}
+	if (str != NULL)
+	{
+		while (str[len])
+			len++;
+	}
 	return (len);
 }
