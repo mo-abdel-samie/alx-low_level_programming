0ef587a3a229efa97a796a260e88b5a31a7bf221
mo-abdel-samie <mohamed.abdelsamie3009@gmail.com>
2023-11-06 00:48:33 -0800

    m

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function that make proces
+ * Description: function for make proces
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Hello World\n");
+	return (0);
+}
